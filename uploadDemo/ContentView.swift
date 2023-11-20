78fb29c11dd699dbdad3906c280ef1bc08edf8c6
VladMihai012 <mocanvlad7@gmail.com>
2023-11-20 21:21:27 +0200

    Initial Commit

---
+++ uploadDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uploadDemo
+//
+//  Created by Mocan Vlad on 20.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uploadDemo/uploadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  uploadDemoApp.swift
+//  uploadDemo
+//
+//  Created by Mocan Vlad on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct uploadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
